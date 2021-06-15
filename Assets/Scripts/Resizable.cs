9037f76e34f0089c25b04d7e8dfa18a28a0f9880
Will Hanstedt <hanstedt@grinnell.edu>
2021-06-15 15:25:40 -0500

    Rewrote "Resizable

--- Assets/Scripts/Resizable.cs
+++ Assets/Scripts/Resizable.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Resizable : MonoBehaviour
 {
@@ -10,10 +11,51 @@ public class Resizable : MonoBehaviour
     [SerializeField] // Find some way to clamp here
     public float radius;
 
-    private void Awake()
+    private bool isGrabbed;
+
+    [SerializeField]
+    InputActionAsset inputActions;
+
+    private InputAction resizeAction;
+
+    private float lastResized;
+
+    private void Start()
     {
+        lastResized = Time.time;
+
         radius = Mathf.Clamp(radius, minRad, maxRad);
         transform.localScale = Vector3.one * radius;
+        isGrabbed = false;
+
+        resizeAction = inputActions.FindActionMap("XRI RightHand").FindAction("Resize Object");
+    }
+
+    private void Update()
+    {
+        if (isGrabbed && (Time.time > lastResized + 0.5) && (resizeAction.phase == InputActionPhase.Started))
+        {
+            float current = resizeAction.ReadValue<Vector2>().y;
+            if (current > 0f)
+            {
+                SizeUp();
+                lastResized = Time.time;
+            } else if(current < 0f)
+            {
+                SizeDown();
+                lastResized = Time.time;
+            }
+        }
+    }
+
+    public void SetGrabbedFalse ()
+    {
+        isGrabbed = false;
+    }
+
+    public void SetGrabbedTrue()
+    {
+        isGrabbed = true;
     }
 
     public void SizeUp()
