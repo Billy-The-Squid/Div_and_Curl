82c485859ae38128f791fdff9b2627527560c8ec
Will Hanstedt <hanstedt@grinnell.edu>
2021-06-16 13:40:26 -0500

    v0.1.1-2: Movement modes

--- Assets/Scripts/MovementManager.cs
+++ Assets/Scripts/MovementManager.cs
@@ -1,21 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class MovementManager : MonoBehaviour
 {
     [SerializeField]
-    
+    ContinuousMoveProviderBase moveProvider;
+    [SerializeField]
+    TeleportManager teleporter;
+
+    private enum MovementMode { Smooth, Teleport }
+    [SerializeField]
+    MovementMode movementMode = MovementMode.Teleport;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        UpdateMovementMode();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateMovementMode()
     {
-        
+        if(movementMode == MovementMode.Smooth)
+        {
+            moveProvider.enabled = true;
+            teleporter.enabled = false;
+        } else
+        {
+            moveProvider.enabled = false;
+            teleporter.enabled = true;
+        }
+    }
+
+    public void SetModeSmooth()
+    {
+        movementMode = MovementMode.Smooth;
+        UpdateMovementMode();
+    }
+
+    public void SetModeTeleport()
+    {
+        movementMode = MovementMode.Teleport;
+        UpdateMovementMode();
     }
 }
