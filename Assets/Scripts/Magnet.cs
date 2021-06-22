37bab792de1bd6a1e933e71f68cffd8188283c76
Will Hanstedt <39772580+Billy-The-Squid@users.noreply.github.com>
2021-06-22 08:46:44 -0500

    Still working...

--- Assets/Scripts/Magnet.cs
+++ Assets/Scripts/Magnet.cs
@@ -24,33 +24,38 @@ public class Magnet : MonoBehaviour
     [SerializeField]
     VectorField field;
 
+    // The arrays used to initialize the argument buffers. 
     [NonSerialized]
     private float[] floatArray = { 2f, 2f, 3f };
     [NonSerialized]
     private Vector3[] vec_array = { new Vector3(0, 0, 0), new Vector3(0, 0, 1), new Vector3(0, 1, 0) };
+    // Eventually, these will be set based on a position
+
+
 
     void Start()
     {
-        unsafe
-        {
+        // Create the ComputeBuffers
+        unsafe {
             floatArgs = new ComputeBuffer(3, sizeof(float));
             vectorArgs = new ComputeBuffer(3, sizeof(Vector3));
         }
 
+        // Initialize the ComputeBuffers
         floatArgs.SetData(floatArray);
         vectorArgs.SetData(vec_array);
 
-        field.preCalculations += DoThing;
+        // VectorFields will call SetExtraArgs before every calculation.
+        field.preCalculations += SetExtraArgs;
     }
-     
-    public void DoThing()
+    
+    /// <summary>
+    /// Sets the extra float and vector arguments of the <cref>VectorField</cref>
+    /// </summary>
+    public void SetExtraArgs()
     {
-        field.floatArgsBuffer = floatArgs; // still crashes
+        field.floatArgsBuffer = floatArgs; 
         field.vectorArgsBuffer = vectorArgs;
-        //field.floatArgsArray = floatArray;
-        //field.vectorArgsArray = vec_array;
-        //Array.Copy(floatArray, field.floatArgsArray, floatArray.Length);
-        //Array.Copy(vec_array, field.vectorArgsArray, vec_array.Length);
     }
 
     // Make sure to wipe the Compute buffers after use. Otherwise, the GPU will complain!
