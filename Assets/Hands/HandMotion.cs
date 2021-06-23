5db83d8b2b774179a3c9800f27646bfb1c236179
Will Hanstedt <39772580+Billy-The-Squid@users.noreply.github.com>
2021-06-23 15:04:20 -0500

    Working hand models!

--- Assets/Hands/HandMotion.cs
+++ Assets/Hands/HandMotion.cs
@@ -19,37 +19,50 @@ public class HandMotion : MonoBehaviour
     [SerializeField]
     Hand hand;
 
+    private Animator animator;
+    int thumbLayerIndex;
+    int pointLayerIndex;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hand detection script is running"); 
+        //Debug.Log("Hand detection script is running"); 
         map = actionAsset.FindActionMap("XRI " + hands[(int)hand] + "Hand");
-        Debug.Log("Found map (" + map.name + "):" + (map != null));
-        map.Enable();
+        //Debug.Log("Found map (" + map.name + "):" + (map != null));
+        //map.Enable();
 
-        pinch = map.FindAction("Pinch");
-        grip = map.FindAction("Grip");
-        pinchTouch = map.FindAction("Pinch Touch");
-        thumbTouch = map.FindAction("Thumb Touch");
+        pinch = map.FindAction("Pinch"); // Value type, axis.
+        grip = map.FindAction("Grip"); // Value type, axis.
+        pinchTouch = map.FindAction("Pinch Touch"); // Button type, float
+        thumbTouch = map.FindAction("Thumb Touch"); // Button type, float
 
-        Debug.Log("Found action: " + pinch.name);
-        Debug.Log("Found action: " + grip.name);
-        Debug.Log("Found action: " + pinchTouch.name);
-        Debug.Log("Found action: " + thumbTouch.name);
+        //Debug.Log("Found action: " + pinch.name);
+        //Debug.Log("Found action: " + grip.name);
+        //Debug.Log("Found action: " + pinchTouch.name);
+        //Debug.Log("Found action: " + thumbTouch.name);
+
+        animator = GetComponent<Animator>();
+        thumbLayerIndex = animator.GetLayerIndex("Thumb Layer");
+        pointLayerIndex = animator.GetLayerIndex("Point Layer");
     }
 
     // Update is called once per frame
     void Update()
     {
-        DebugAction(pinch);
+        //DebugAction(pinch);
+        animator.SetFloat("Flex", grip.ReadValue<float>());
+        animator.SetFloat("Pinch", pinch.ReadValue<float>()); // Does this need modification?
+
+        animator.SetLayerWeight(thumbLayerIndex, 1f - thumbTouch.ReadValue<float>()); // Or is it 1-x?
+        animator.SetLayerWeight(pointLayerIndex, 1f - pinchTouch.ReadValue<float>());
     }
 
     void DebugAction(InputAction action)
     {
         Debug.Log("Map (" + map.name + ") Action (" + action.name + ")");
-        //Debug.Log("Read value (float): " + action.ReadValue<float>());
+        Debug.Log("Read value (float): " + action.ReadValue<float>());
         //Debug.Log("Read value (bool): " + action.ReadValue<bool>());
-        //Debug.Log("Triggered? " + action.triggered);
+        Debug.Log("Triggered? " + action.triggered);
         Debug.Log("Phase: " + action.phase);
     }
 }
